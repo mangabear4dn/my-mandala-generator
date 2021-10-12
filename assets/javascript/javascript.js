b25ac59fcedbcc036075c4c92d54094959f92ba4
mangabear4dn <luize.blake@gmail.com>
2021-10-12 17:51:31 +0000

    Add function that turns today's date to ddmmyyyy string

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,1 +1,40 @@
-console.log('hello hello');
+/**
+ * This document contails javascript code used in index.html document of this project.
+*/
+/* 
+  References for manipulations of the date:
+  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+  * notes of the CI course
+*/
+
+function getDateString(){
+  /* getting the date string ddmmyyyy */
+  let date, month, year;
+
+  /* 
+    for testing purposes of functions 
+    for now only using today's date
+  */
+  let dateInput = new Date; // date is the today's date
+
+  // dd
+  date = dateInput.getDate().toString();
+
+  // mm (0 needed for january till september)
+  month = Number.parseInt(dateInput.getMonth()); 
+  month++; // getMonth() starts counting from 0 as the first
+  let m = Number.parseInt(month);
+  if (m < 10){
+    month = '0' + month;
+  }
+  month = month.toString();
+
+  // yyyy
+  year = dateInput.getFullYear().toString(); 
+  
+  // dateString
+  let dateString = date + month + year;
+  return dateString;
+}
+
+console.log(getDateString());
